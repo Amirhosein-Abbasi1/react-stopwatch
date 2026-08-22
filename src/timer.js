8a9cb7b0559eb0847800b9f6b543012aff012b87
Amirhosein <amirhabbasi2007@gmail.com>
2026-08-22 19:08:39 +0330

    06 - Timer component numbers

--- src/timer.js
+++ src/timer.js
@@ -1,9 +1,29 @@
-const Timer = () => {
+import React from "react";
+import { useState } from "react";
 
-    return (
+const Timer = () => {
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [milliSecond, setMilliSecond] = useState(0);
+
+  let m = minute;
+  let s = second;
+  let milli = milliSecond;
+
+  return (
     <div className="timer-component-main-div">
-        
+
+      <div className="timer-section">
+        <p className="minute-and-second">
+          {` ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s} `}
+        </p>
+        <p className="milisecond">
+          {`. ${milli > 9 ? milli : "0" + milli}`}
+        </p>
+      </div>
+
+      <div className="buttons-section"></div>
     </div>
-    )
-}
+  );
+};
 export default Timer;
