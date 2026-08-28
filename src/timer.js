ed9a9427c04c26bd0deceb8f8663e2b632e3e7bb
Amirhosein <amirhabbasi2007@gmail.com>
2026-08-28 16:55:18 +0330

    08 - Start and Stop timer

--- src/timer.js
+++ src/timer.js
@@ -1,15 +1,58 @@
-import React from "react";
-import { GrFlagFill, GrPowerReset, GrPlayFill } from "react-icons/gr";
+import React, { useRef } from "react";
+import {
+  GrFlagFill,
+  GrPowerReset,
+  GrPlayFill,
+  GrPauseFill,
+} from "react-icons/gr";
 import { useState } from "react";
 
 const Timer = () => {
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
   const [milliSecond, setMilliSecond] = useState(0);
+  const [isStart, setIsStart] = useState(false);
+
+  const timerRef = useRef(null);
 
   let m = minute;
   let s = second;
   let milli = milliSecond;
+
+  const startTimer = () => {
+    if (isStart) return;
+    setIsStart(true);
+
+    timerRef.current = setInterval(() => {
+      setMilliSecond((prev) => {
+        if (prev === 99) {
+          setSecond((prev) => prev + 1);
+          return 0;
+        }
+        return prev + 1;
+      });
+
+      setSecond((prev) => {
+        if (prev === 60) {
+          setMinute((prev) => prev + 1);
+          return 0;
+        }
+        return prev;
+      });
+
+      setMinute((prev) => {
+        if (prev === 60) {
+          return 0;
+        }
+        return prev;
+      });
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
+    setIsStart(false);
+  };
 
   return (
     <div className="timer-component-main-div">
@@ -28,8 +71,13 @@ const Timer = () => {
           <p>Lap</p>
         </div>
 
-        <button className="start-btn" type="button">
-          <GrPlayFill /> Start
+        <button
+          className="start-btn"
+          type="button"
+          onClick={isStart ? stopTimer : startTimer}
+        >
+          {isStart ? <GrPauseFill /> : <GrPlayFill />}
+          {isStart ? "Stop" : "Start"}
         </button>
 
         <div className="btn-with-title">
